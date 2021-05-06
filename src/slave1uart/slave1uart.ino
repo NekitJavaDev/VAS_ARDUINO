546067956a7d92ca2bc0ef4b402ffc7934f26b90
NekitJavaDev <blake_nik@icloud.com>
2021-05-06 17:15:17 +0300

    Change structure of response to JSON. Request all sensors now only when master device send request to slave (don't every 1 seconds). Remove Dallass temperature sensor and change to DHT11 sensor to read temperature plus humidity

---
+++ src/slave1uart/slave1uart.ino
@@ -0,0 +1,91 @@
+#include <ArduinoJson.h>
+#include "DHT.h"
+
+#define RS485_ID 1  // ID slave устройства (уникальный номер, чтобы различать устройства системы)
+#define RS485_DIR 10  // переключение RS485 приёмника/передатчика: LOW - "приём данных", HIGH - "передача" (RS-485 to TTL MAX)
+
+#define GERKON_PIN 4  // геркон (values: 1 = "open", 0 = "close")
+
+#define DHT_PIN 2  // датчик температуры и влажности (DHT11)
+
+#define SMOKE_SENSOR_PIN A0  // датчик дыма (MQ-2)
+
+#define VOLTAGE_PIN A4  // датчик измерения входного напряжения (Voltage Sensor)
+#define R1_VOLTAGE 30000.0  // R1(Voltage Sensor)
+#define R2_VOLTAGE 7500.0  // R2(Voltage Sensor) 
+float U_REF = R2_VOLTAGE / (R1_VOLTAGE + R2_VOLTAGE);  // вычисление входного напряжения (max) (Voltage Sensor)
+float U_HIGH = 5.0;
+
+DHT dht(DHT_PIN, DHT11);
+
+StaticJsonDocument<200> slave;
+
+void setup() 
+{  
+    Serial.begin(115200);  // UART на 115200 бод (max скорость обмена с ПК)
+    while (!Serial) {
+        ;  // wait for serial port to connect. Needed for native USB
+    }
+    
+    dht.begin();
+    
+    pinMode(GERKON_PIN, INPUT);
+    digitalWrite(GERKON_PIN, HIGH);
+    
+    pinMode(RS485_DIR, OUTPUT);
+    digitalWrite(RS485_DIR, LOW); 
+}
+
+void loop() 
+{                                                 
+    while (Serial.available() > 0) {  // Если есть какие либо данные в последовательном порту и ID = 1
+      String strId = Serial.readString();      
+      if (strId == "1") {  // Проверям, что запрос пришёл имеено на нужное slave устройство
+          digitalWrite(RS485_DIR, HIGH);
+          delay(10);
+
+          startPollingSensors();
+          serializeJson(slave, Serial);
+          
+          delay(10);      
+          digitalWrite(RS485_DIR, LOW);      
+      } else {
+          return;  // Иначе выходи из loop() и просто ждём новых данных   
+      }          
+    }
+}
+
+/**
+*  Example response to send to MASTER device
+*  {"id":1,"temp":23.6,"humidity":37,"door_is":"open","voltage":1.196289,"smoke":881}
+*/
+void startPollingSensors()
+{
+    slave["id"] = RS485_ID;  
+    slave["temp"] = 0.0;
+    slave["humidity"] = 0;
+    slave["door_is"] = "error";
+    slave["voltage"] = 0.0;
+    slave["smoke"] = 0;
+
+    int doorIsOpen = digitalRead(GERKON_PIN);
+    String convertIsOpenDoorValue = doorIsOpen ? "open" : "close";
+    slave["door_is"] = convertIsOpenDoorValue;   
+    
+    float uOut = (analogRead(VOLTAGE_PIN) * U_HIGH) / 1024.0;
+    float voltage = uOut / U_REF;
+    slave["voltage"] = voltage;
+    
+    int smoke = analogRead(SMOKE_SENSOR_PIN);  // 100-500 нормальная концентрация, > 600 - WARNING!
+    slave["smoke"] = smoke;
+
+    int humidity = dht.readHumidity();  
+    float temp = dht.readTemperature();  
+    if (isnan(humidity) || isnan(temp)) {  
+        slave["temp"] = 0.0;
+        slave["humidity"] = 0;    
+    } else {
+        slave["temp"] = temp;
+        slave["humidity"] = humidity;   
+    }
+}
