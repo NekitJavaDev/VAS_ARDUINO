87dc5ae8d818ff12131bf0cf9d35326a6d56c6a3
NekitJavaDev <blake_nik@icloud.com>
2021-05-07 11:41:23 +0300

    Change master device from NANO to MEGA 2560. Improved the algorithm for polling slave(remote) devices. Use UART port to send request (Serial3).Increase the speed of data exchange from 4800 baud (bit per seconds) to 115200 - max speed to PC COM port.

---
+++ src/masternewuart/masternewuart.ino
@@ -0,0 +1,231 @@
+#include <LiquidCrystal_I2C.h>
+#include <ArduinoJson.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);  // Для экрана 20х4 (четырехстрочный)
+
+#define DIR 10  //отправка RS485
+
+//#define GERKON_LED_PIN_DEVICE_1 A1  // slave1 
+//#define SMOKE_LED_PIN_DEVICE_1 A3  // slave1
+//#define GERKON_LED_PIN_DEVICE_2 A2  // slave2
+//#define SMOKE_LED_PIN_DEVICE_2 A5  // slave2
+//#define GERKON_LED_PIN_DEVICE_2 A2  // slave3
+//#define SMOKE_LED_PIN_DEVICE_2 A5  // slave3
+
+#define interval 10000  //5000
+
+unsigned long previousMillis = 0;
+int IDsend = 0;  // Счётчик для опроса устройств
+
+StaticJsonDocument<200> slave;  // Пришедший JSON со slave устройства 
+
+void setup()
+{
+    Serial3.begin(115200);  // UART port, which send and take from slave devices
+    Serial.begin(115200);
+
+    // pinMode(GERKON_LED_PIN_DEVICE_1, OUTPUT);
+    // pinMode(SMOKE_LED_PIN_DEVICE_1, OUTPUT);
+    // pinMode(GERKON_LED_PIN_DEVICE_2, OUTPUT);
+    // pinMode(SMOKE_LED_PIN_DEVICE_2, OUTPUT);
+    
+    pinMode(DIR, OUTPUT);
+    digitalWrite(DIR, LOW);
+    
+    lcd.begin();
+    lcd.setCursor(6, 0);  // Центр первой строки
+    lcd.print("Devices:");
+    lcd.setCursor(0, 1);  // Device 1
+    lcd.print("1");
+    lcd.setCursor(0, 2);  // Device 2
+    lcd.print("2");
+    lcd.setCursor(0, 3);  // Device 3
+    lcd.print("3");
+}
+
+void loop()
+{
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis > interval) {
+        previousMillis = currentMillis;
+        if (previousMillis > currentMillis) {
+            previousMillis = 0;
+        }
+
+        IDsend++;
+        Serial.print("ID send = ");
+        Serial.println(IDsend);
+
+        switch (IDsend) {
+            case 1:
+              lcd.setCursor(1, 1);
+              lcd.print("                   ");  // Очистка 1-ой строки  
+              lcd.setCursor(2, 1);
+              lcd.print("Send to Device 1");
+              sendCmdRS485(IDsend, 1);
+            break;
+            case 2:
+              lcd.setCursor(1, 2);
+              lcd.print("                   ");  // Очистка 2-ой строки  
+              lcd.setCursor(2, 2);
+              lcd.print("Send to Device 2");
+              sendCmdRS485(IDsend, 1);
+              IDsend = 0;
+            break;
+            default:
+              Serial.println("DEFAULT HAPPENED");
+            break;
+        }
+      
+    }
+
+    delay(10);
+
+    /**
+    *  Приём данных, обработка и вывод на LCD 
+    */
+    while (Serial3.available() > 0) {    
+        DeserializationError error = deserializeJson(slave, Serial3);    
+  //        if (error) {  // Test if parsing succeeds.
+  //          Serial.print(F("deserializeJson() failed: "));
+  //          Serial.println(error.f_str());
+  //          return;
+  //        }
+        char outStrTemperature[4];
+        char outStrVoltage[3];
+          
+        float temp = slave["temp"];
+        String doorIs = slave["door_is"];
+        float voltage = slave["voltage"];
+        int smoke = slave["smoke"];
+        int humidity = slave["humidity"];
+
+        Serial.println("I recieved JSON:");
+        int id = slave["id"];  
+
+        switch (id) {
+        case 0:
+            Serial.println("Error deserialize Json!! ID = 0");
+            return;          
+        case 1:
+            // turnOnOrOffLedGerkonPins(1, doorIs);
+            // turnOnOrOffLedSmokePins(1, smoke);
+            
+            lcd.setCursor(0,1);
+            lcd.print("                    "); //Очистка 1-ой строки
+            lcd.setCursor(0,1);
+            lcd.print("1 T:" + String(dtostrf(temp,2,1,outStrTemperature)) + " V:" + String(dtostrf(voltage,2,1,outStrVoltage)) + " H:" + humidity + "%"); 
+        break;
+        case 2:
+            // turnOnOrOffLedGerkonPins(2, doorIs);
+            // turnOnOrOffLedSmokePins(2, smoke);
+            lcd.setCursor(0,2);
+            lcd.print("                    "); //Очистка 2-ой строки
+            lcd.setCursor(0,2);
+            lcd.print("2 T:" + String(dtostrf(temp,2,1,outStrTemperature)) + " V:" + String(dtostrf(voltage,2,1,outStrVoltage)) + " H:" + humidity + "%"); 
+        break;
+        case 3:
+            lcd.setCursor(0,3);
+            lcd.print("                    "); //Очистка 2-ой строки
+            lcd.setCursor(0,3);
+            lcd.print("3 T:" + String(dtostrf(temp,2,1,outStrTemperature)) + " V:" + String(dtostrf(voltage,2,1,outStrVoltage)) + " H:" + humidity + "%"); 
+        break;
+        default:
+            Serial.print("Incorrect input ID from slave device = ");
+            Serial.println(id);
+        break;
+        }
+        delay(10);
+        
+        Serial.println("After parsing JSON:");
+        Serial.print("Slave ID: ");
+        Serial.println(id);
+        Serial.print("Temperature: ");
+        Serial.println(temp, 1);
+        Serial.print("Door is: ");
+        Serial.println(doorIs);
+        Serial.print("Voltage: ");
+        Serial.println(voltage);
+        Serial.print("Smoke: ");
+        Serial.println(smoke);
+        Serial.print("Humidity: ");
+        Serial.println(humidity);   
+    }
+  
+}
+
+/**
+   Метод для отправки запроса по RS485
+*/
+void sendCmdRS485(int id, int cmd)
+{
+    digitalWrite(DIR, HIGH);
+    delay(10);
+    
+    Serial.println("SENDING to device with ID = " + String(id) + "cmd = " + String(cmd));    
+    Serial3.print(id);   //последовательно передаем ID slave устройства через шину RS-485 на UART
+
+    delay(10);
+    digitalWrite(DIR, LOW);
+}
+
+
+/*
+void turnOnOrOffLedGerkonPins(int deviceId, String doorIs) {
+    switch (deviceId) {
+        case 1:
+            if (doorIs == "open") {
+              digitalWrite(GERKON_LED_PIN_DEVICE_1, HIGH);
+            } else if (doorIs == "close") {
+              digitalWrite(GERKON_LED_PIN_DEVICE_1, LOW);
+            } else {
+              Serial.print("Error turning on/off LED GERKON pin with ID device = ");
+              Serial.print(deviceId);
+              Serial.print(" and door's GERKON value = "); 
+              Serial.println(doorIs);                   
+            }
+        break;      
+        case 2:
+            if (doorIs == "open") {
+              digitalWrite(GERKON_LED_PIN_DEVICE_2, HIGH);
+            } else if (doorIs == "close") {
+              digitalWrite(GERKON_LED_PIN_DEVICE_2, LOW);
+            } else {
+              Serial.print("Error turning on/off LED GERKON pin with ID device = ");
+              Serial.print(deviceId);
+              Serial.print(" and door's GERKON value = "); 
+              Serial.println(doorIs);            
+            }
+        break;
+        default:
+            Serial.print("Error turning on/off LED GERKON pin with ID device = ");
+            Serial.println(deviceId);  
+        break;
+    }
+}
+
+void turnOnOrOffLedSmokePins(int deviceId, int smoke) {
+    switch (deviceId) {
+        case 1:
+            if (smoke > 700) {
+              digitalWrite(SMOKE_LED_PIN_DEVICE_1, HIGH);
+            } else {
+              digitalWrite(SMOKE_LED_PIN_DEVICE_1, LOW);
+            }
+        break;      
+        case 2:
+          if (smoke > 700) {
+            digitalWrite(SMOKE_LED_PIN_DEVICE_2, HIGH);
+          } else {
+            digitalWrite(SMOKE_LED_PIN_DEVICE_2, LOW);
+          }
+        break;
+        default:
+            Serial.print("Error turning on/off SMOKE GERKON pin with ID device = ");
+            Serial.println(deviceId);  
+        break;
+    }
+}
+
+*/
